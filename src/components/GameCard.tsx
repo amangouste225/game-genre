3dbd73490bd22160790f7aafb648d3d6b78b8efc
Amangoua Ange <adagou@yahoo.com>
2023-11-27 19:13:36 +0000

    update

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const GameCard = ({ game }: Props) => {
   return (
-    <div className='card max-w-[380px] h-[260px] bg-base-300 shadow-lg cursor-pointer hover:scale-105'>
+    <div className='card max-w-[380px] h-[260px] bg-base-300 shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-300'>
       <figure className='h-full w-full'>
         <img
           src={game.background_image}
