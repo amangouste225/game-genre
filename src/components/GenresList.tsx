d820e0fcca28ed2c750d68171c99c9ee039274d0
Amangoua Ange <adagou@yahoo.com>
2023-11-28 14:11:01 +0000

    genres list

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,15 +1,22 @@
 import { useGenres } from '../hooks/useGenres';
+import { getCropped } from '../services/image-url';
 
 export const GenresList = () => {
   const { genres } = useGenres();
 
   return (
-    <ul className='menu p-4 w-60 min-h-full bg-base text-base-content'>
+    <div className='menu p-4 flex gap-5 w-60 min-h-full bg-base text-base-content'>
       {genres.map((genre) => (
-        <li key={genre.id}>
-          <a href=''>{genre.name}</a>
-        </li>
+        <div key={genre.id} className='flex items-center gap-2 group'>
+          <img
+            src={getCropped(genre.image_background)}
+            className='max-h-[25px] rounded-md'
+          />
+          <a href='#' className='group-hover:underline'>
+            {genre.name}
+          </a>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
